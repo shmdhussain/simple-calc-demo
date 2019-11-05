931c385ed0320124d0c98b37f695773183ea489f
shmdhussain <shmdhussain12@yahoo.com>
2019-11-05 17:10:05 +0400

    testing failed scenario

--- test/app.test.js
+++ test/app.test.js
@@ -4,7 +4,7 @@ import {strict as assert} from 'assert';
 // import { calculateFns } from '../src/app';
 
 test('My test', t => { 
-	assert.equal('abc', 'abc'); 
+	assert.equal('sabc', 'abc'); 
 });
 
 for (var i = 0; i < 3; i++) {
