9be42e93978bca7e2229d18ca1a567cac640932c
Mohamed Hussain S H <mohamed.hussain@skynewsarabia.com>
2022-08-16 18:09:42 +0400

    added comment as requested

--- app/arithmetics.js
+++ app/arithmetics.js
@@ -2,6 +2,7 @@ var calculations = {};
 calculations.add = function (a, b){
 	return a+b;
 }
+//sub is new feature added comment from the test account
 calculations.sub = function (a, b){
 	return a-b;
 }
